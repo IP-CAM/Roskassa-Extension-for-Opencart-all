95a0fea851afee4bb1f6bae91aa00f28025e833b
syntlex-Biz <77607696+syntlex-Biz@users.noreply.github.com>
2021-01-30 20:16:36 +0600

    add plugin

---
+++ Opencart/upload/admin/language/en-gb/extension/payment/roskassa.php
@@ -0,0 +1,38 @@
+<?php
+// Heading
+$_['heading_title']		            = 'Roskassa';
+
+// Text
+$_['text_extension']	            = 'Extensions';
+$_['text_success']		            = 'Success: You have modified Roskassa account details!';
+$_['text_edit']                     = 'Edit Roskassa';
+$_['text_roskassa']	            = '<a href="https://roskassa.net/" target="_blank"><img src="view/image/payment/roskassa.svg" alt="Roskassa" title="Roskassa" style="border: 1px solid #EEEEEE;" /></a>';
+$_['text_info_ssl']                 = 'For payment with Apple Pay to work, your site must be using HTTPS (valid TLS / SSL certificate)';
+$_['text_info_support']             = 'Please contact Roskassa commercial department for additional settings for Apple Pay';
+
+// Entry
+$_['entry_account']		            = 'Account ID';
+$_['entry_secret']		            = 'Account Code';
+$_['entry_test']		            = 'Test Mode';
+$_['entry_bank_cards']	            = 'Bank cards - priority payment method';
+$_['entry_total']		            = 'Total';
+$_['entry_order_status']            = 'Order Status';
+$_['entry_geo_zone']	            = 'Geo Zone';
+$_['entry_status']		            = 'Status';
+$_['entry_sort_order']	            = 'Sort Order';
+$_['entry_ap_use']                  = 'Apple Pay';
+$_['entry_ap_public_id']            = 'Public ID';
+$_['entry_ap_payee']                = 'Payment receiver';
+
+// Help
+$_['help_secret']		            = 'The secret word to confirm transactions with (must be the same as defined on the merchant account configuration page).';
+$_['help_total']		            = 'The checkout total the order must reach before this payment method becomes active.';
+$_['help_bank_cards']               = 'The buyer will be asked to enter the bank card details. Immediately on your website page';
+$_['help_ap_use']                   = 'Connecting Apple Pay is a contactless payment method. Only works for Apple devices';
+$_['help_ap_public_id']             = 'Public identifier (Public Id) of your account from Roskassa';
+$_['help_ap_payee']                 = 'Russian letters cannot be used. For example - the name of your online store. Will be displayed to the buyer during the payment process';
+
+// Error
+$_['error_permission']	 = 'Warning: You do not have permission to modify payment Roskassa!';
+$_['error_account']		 = 'Account No. Required!';
+$_['error_secret']		 = 'Account Code Required!';
